42fdec8302b904f90bce329d00b867e7af1ffcd0
Muhammed Karaman <52467541+midriskaraman@users.noreply.github.com>
2020-01-17 18:48:30 +0300

    Header
    internet sayfamın header kısmını yazdığımız kodlar

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>film oneri projemiz</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <link href="css/awesome.css" rel="stylesheet">
+    <style type="text/css">
+    .container{
+    background-color:rgba(255, 255, 255, 0.5);
+    padding:30px;
+    }
+    
+    body{
+    background-image:url('img/background1.png');
+    background-size: cover;
+  -moz-background-size: cover;  /* Firefox 3.6 */
+  background-position: center;  /* Internet Explorer 7/8 */
+  background-attachment: fixed;
+    }
+    #box{
+    -webkit-box-shadow: 4px 4px 23px -6px rgba(0,0,0,0.48);
+	-moz-box-shadow: 4px 4px 23px -6px rgba(0,0,0,0.48);
+	box-shadow: 4px 4px 23px -6px rgba(0,0,0,0.48);
+	padding:20px;
+	border:2px solid #ccc;
+	background-color:rgba(255, 255, 255, 0.5);
+    }
+    </style>
+
+</head>
+  <body>
